390cc505aa9b1569c90b6877347647f8e95b4536
Tombeau Chrisnor <tombeauc@gmail.com>
2024-01-09 04:52:39 -0500

    nav bar for mobile

--- src/app/components/shared/template/header/header.component.ts
+++ src/app/components/shared/template/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+     
+    actionMenu()
+    {
+       console.log("Test bouton");
+    }
 }
