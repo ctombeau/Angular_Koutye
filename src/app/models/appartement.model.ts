13f42f122ea2d050da09d2a38cfac36a6aa78996
Tombeau Chrisnor <tombeauc@gmail.com>
2024-08-07 21:36:27 -0400

    getAppartement 1

---
+++ src/app/models/appartement.model.ts
@@ -0,0 +1,17 @@
+import { Adresse } from "./adresse.model";
+import { User } from "./user.model";
+
+export class Appartement{
+    //id : number;
+    description: string;
+    adresse : Adresse;
+    user : User;
+    
+    constructor(description: string,adresse:Adresse, user:User){
+        //this.id = id;
+        this.description = description;
+        this.adresse=adresse;
+        this.user=user;
+    }
+
+}
--- src/app/services/appartement.service.ts
+++ src/app/services/appartement.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable,catchError,map,of } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { Appartement } from '../models/appartement.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppartementService {
+  private baseApi = environment.apiUrl;
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+   public showByUsername(username: string) : Observable<Appartement[]>{
+       return this.http.get<Appartement[]>(this.baseApi+"appartement/show-by-commune?username="+username).pipe(
+          map((apps: Appartement[]) =>apps.map(app=>({
+                ...app
+            
+          })as Appartement)),
+          catchError((err:HttpErrorResponse)=>{
+              if(err.status){
+
+              }
+              return [];
+          })
+       );
+   }
 }
